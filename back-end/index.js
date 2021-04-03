7f71325dbdb7b887f90c8cc590668f4e83a22449
Niskaaaa <1162314353@qq.com>
2021-04-03 23:30:04 +0800

    pretty

--- back-end/index.js
+++ back-end/index.js
@@ -2,20 +2,25 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const cors = require("@koa/cors");
 const koaBody = require("koa-body");
+const json = require("koa-json");
 const app = new Koa();
 const router = new Router();
 
-router.prefix('/api')
+router.prefix("/api");
 router.get("/", (ctx) => {
   console.log(ctx);
   ctx.body = "Hello World";
 });
 
-router.get("/api", (ctx) => {
-  const params = ctx.request.query
-  console.log(params.name)
+router.get("/ai", (ctx) => {
   console.log(ctx);
-  ctx.body = "Hello api";
+  ctx.body = "Hello World";
+});
+
+router.get("/api", (ctx) => {
+  const params = ctx.request.query;
+  console.log(params);
+  ctx.body = {name:params.name}
 });
 router.get("/async", async (ctx) => {
   let result = await new Promise((resolve) => {
@@ -27,12 +32,12 @@ router.get("/async", async (ctx) => {
 });
 
 router.post("/post", async (ctx) => {
-  let{body} = ctx.request;
-  console.log(body)
-  ctx.body={...body}
+  let { body } = ctx.request;
+  console.log(body);
+  ctx.body = { ...body };
 });
-app.use(koaBody())
-app.use(cors())
+app.use(koaBody());
+app.use(json({pretty:false,param:'pretty'}))
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
-
 app.listen(3000);
