b6b7f0338ebfdba6a42ece522b15f29134c639bc
Niskaaaa <1162314353@qq.com>
2021-04-04 22:25:20 +0800

    improve

---
+++ back-end/src/api/b.js
@@ -0,0 +1,7 @@
+
+
+module.exports =function(ctx){
+    ctx.body = {
+        "message":"hello,b"
+    }
+}
---
+++ back-end/src/routes/routes.js
@@ -0,0 +1,7 @@
+const combineRoutes = require("koa-combine-routers");
+
+const aroutes = require("./aRouter");
+
+const broutes = require("./bRouter");
+
+module.exports = combineRoutes(aroutes, broutes);
